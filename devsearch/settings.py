03ef15277f2f77a54e8230f508460865a64a9a78
Rufus-25 <taiworufuscovenant@gmail.com>
2026-06-26 01:03:09 +0100

    Registration email configured

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -12,19 +12,27 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 import os
+import environ #for environment variables
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Initialize the environment for variables
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+
+# Read the environment
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9ey#2+d91$l^5u_6zu-d(@lz4evjaw*$=bx@e#(s94u97ai&@0'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -117,6 +125,18 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587 Unavailable after running curl -v smtp.gmail.com:587 in CMD
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+
 
 
 # Static files (CSS, JavaScript, Images)
--- users/signals.py
+++ users/signals.py
@@ -1,20 +1,36 @@
 from django.db.models.signals import post_delete, post_save
 from django.contrib.auth.models import User
 
+from django.core.mail import send_mail
+from django.conf import settings
+
 from .models import Profile
 
 
+# Create a profile and send an email when new user is created
 def createProfile(sender, instance, created, **kwargs):
     if created:
         user = instance
-        Profile.objects.create(
+        profile = Profile.objects.create(
             user=user,
             username = user.username,
             email = user.email,
             name = user.first_name,
             )
+        
+        subject = "Welcome to DevSearch!"
+        body = "Congrats! You've successfully signed up on DevSearch. You can go ahead and complete your profile."
+
+        send_mail(
+            subject,
+            body,
+            settings.EMAIL_HOST_USER,
+            [profile.email],
+            fail_silently=False,
+        )
 
 
+# Update core user details when updated in profile
 def updateUser(sender, instance, created, **kwargs):
     profile = instance
     user = profile.user
@@ -26,6 +42,7 @@ def updateUser(sender, instance, created, **kwargs):
         user.save()
 
 
+# Delete a user once the profile is deleted
 def deleteUser(sender, instance, **kwargs):
     try:
         user = instance.user
